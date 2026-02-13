9e3f366eb67eeb561a4859128a8b556d6bf52f25
b47633cw <b47633cw@manchester.ac.uk>
2026-02-13 12:15:53 +0000

    Completed Booking class

---
+++ Lab2/src/Booking.java
@@ -0,0 +1,42 @@
+public class Booking{
+    private LocalDate startDate;
+    private int numOfNights;
+    private double costPerNight;
+
+    public Booking(LocalDate data, int numOfNights, double costPerNight){
+        this.startDate = date;
+        this.numOfNights = numOfNights;
+        this.costPerNight = costPerNight;
+    }
+
+
+    public LocalDate getStartDate(){
+        return this.startDate;
+    }
+
+    public void setStartDate(LocalDate date){
+        this.startDate = date;
+    }
+
+    public int getNumberOfNights(){
+        return this.numOfNights;
+    }
+
+    public void setNumberOfNights(int nights){
+        this.numOfNights = nights;
+    }
+
+    public double getCostPerNight(){
+        return costPerNight;
+    }
+
+    public void setCostPerNight(double cost){
+        this.costPerNight = cost;
+    }
+
+    public double getTotalCost(){
+        return round((this.costPerNight * (double) this.numOfNights), 2);
+    }
+
+
+}
